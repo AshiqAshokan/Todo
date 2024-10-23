e1d00424ac482b18d43486d7c8d66cefdaad7435
AshiqAshokan <149768834+AshiqAshokan@users.noreply.github.com>
2024-10-23 15:52:25 +0530

    Update apiSlice.js

--- Frontend/src/Slice/apiSlice.js
+++ Frontend/src/Slice/apiSlice.js
@@ -1,7 +1,7 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: '',
+  baseUrl: 'https://todo-backend-85lk.onrender.com',
   prepareHeaders: (headers) => {
     const token = localStorage.getItem('accessToken')
     
