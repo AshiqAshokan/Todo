bed9ccb57b1789be7adab4eae3ff773d2dbc1d13
AshiqAshokan <149768834+AshiqAshokan@users.noreply.github.com>
2024-10-24 09:24:58 +0530

    Update UserRegistration.jsx

--- Frontend/src/Components/UserRegistration.jsx
+++ Frontend/src/Components/UserRegistration.jsx
@@ -152,25 +152,15 @@ const UserRegistration = () => {
             </button>
 
              <GoogleOAuthProvider clientId="1038818047013-kbkd3pndcmp5p0hp79u9hvea1men57ip.apps.googleusercontent.com">
-                  <div className="w-full">
+                 
               
                <GoogleLogin
                   onSuccess={handleGoogleSuccess}
                   onFailure={handleGoogleFailure}
                   buttonText="Sign Up with Google"
-                  style={{
-                      backgroundColor: '#4285F4', // Google blue
-                      color: 'white',
-                      padding: '10px 20px',
-                      border: 'none',
-                      borderRadius: '5px',
-                      cursor: 'pointer',
-                      fontSize: '16px',
-                      display: 'flex',
-                      alignItems: 'center',
-                    }}
+                  className="!w-full flex items-center justify-center text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2"
                  />
-                      </div>
+  
               </GoogleOAuthProvider>
           </form>
         </div>
