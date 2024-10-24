bd5b73211a7ee0101b60c28e6d963a2ff4f93264
AshiqAshokan <149768834+AshiqAshokan@users.noreply.github.com>
2024-10-24 08:32:37 +0530

    Update userRoutes.js

--- Backend/Routes/userRoutes.js
+++ Backend/Routes/userRoutes.js
@@ -1,9 +1,10 @@
 const Router = require("express").Router()
-const { registerUser,authUser,googleSignIn } = require("../Controller/userController")
+const { registerUser,authUser,googleSignIn,googleLogin } = require("../Controller/userController")
 const protect = require("../middleware/authMiddleware")
 
 Router.post('/register',registerUser)
 Router.post('/login',authUser) 
 Router.post('/google-signin', googleSignIn);
+Router.post('/google-signin',googleLogin);
 
 module.exports=Router
