877afa300725e0b16cdf77f0885ea6aca28ff9e6
AshiqAshokan <149768834+AshiqAshokan@users.noreply.github.com>
2024-10-24 09:25:16 +0530

    Update UserRegistration.jsx

--- Frontend/src/Components/UserRegistration.jsx
+++ Frontend/src/Components/UserRegistration.jsx
@@ -158,7 +158,7 @@ const UserRegistration = () => {
                   onSuccess={handleGoogleSuccess}
                   onFailure={handleGoogleFailure}
                   buttonText="Sign Up with Google"
-                  className="!w-full flex items-center justify-center text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2"
+                  className="w-full flex items-center justify-center text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2"
                  />
   
               </GoogleOAuthProvider>
